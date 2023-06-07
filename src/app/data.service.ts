002cf13046ae84a6f23a7470ced5ae988371e578
zzzzzzl <zzzzzzl@foxmail.com>
2023-06-07 14:35:25 +0900

    分页成功

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -15,60 +15,6 @@ export class DataService {
     return this.http.get<any[]>(`${this.baseUrl}/schools`);
   }
 
-  // filterByOptions(schools: string[], provinces: string[]): Observable<any[][]> {
-  //   const url = `${this.baseUrl}/filteredByOptions`;
-  //   const params = new HttpParams()
-  //     .set('schools', schools.join(','))
-  //     .set('provinces', provinces.join(','));
-
-  //   return this.http.get<any[][]>(url, { params });
-  // }
-
-  // getTableData(tableName: string, page: number, pageSize: number): Observable<any[][]> {
-  //   const url = `${this.baseUrl}/table-data`;
-  //   const params = new HttpParams()
-  //     .set('tableName', tableName)
-  //     .set('page', String(page))
-  //     .set('pageSize', String(pageSize));
-
-  //   return this.http.get<any[][]>(url, { params }).pipe(
-  //     tap(data => {
-  //       if (data.length > 0) {
-  //         this.getDistinctValues(data);
-  //       }
-  //     })
-  //   );
-  // }
-
-
-
-
-  // distinctSchools: string[] = [];
-  // distinctProvinces: string[] = [];
-
-  // // ...
-
-  // getDistinctValues(tableData: any[][]): void {
-  //   const schoolsSet = new Set<string>();
-  //   const provincesSet = new Set<string>();
-
-  //   tableData.forEach(row => {
-  //     schoolsSet.add(row[0]);
-  //     provincesSet.add(row[1]);
-  //   });
-
-  //   this.distinctSchools = Array.from(schoolsSet);
-  //   this.distinctProvinces = Array.from(provincesSet);
-
-  //   console.log('Distinct Schools:', this.distinctSchools);
-  //   console.log('Distinct Provinces:', this.distinctProvinces);
-  // }
-
-  // filterSchools(params: any): Observable<any[]> {
-  //   const url = `${this.baseUrl}/filteredSchools`;
-  //   return this.http.get<any[]>(url, { params });
-  // }
-
 
 }
 
--- src/app/gk2022/gk2022.component.ts
+++ src/app/gk2022/gk2022.component.ts
@@ -8,7 +8,13 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./gk2022.component.css']
 })
 export class Gk2022Component implements OnInit {
+
   combinedColumns: any[] = [];
+  currentPage: number = 1;
+  itemsPerPage: number = 150;
+  totalPages: number = 1;
+  pages: number[] = [];
+  displayedColumns: any[] = [];
 
   constructor(private http: HttpClient) { }
 
@@ -20,6 +26,9 @@ export class Gk2022Component implements OnInit {
     this.http.get<any[]>('http://localhost:3000/combinedColumns').subscribe(
       (response) => {
         this.combinedColumns = response;
+        this.totalPages = Math.ceil(this.combinedColumns.length / this.itemsPerPage);
+        this.updatePages();
+        this.getDisplayedColumns();
       },
       (error) => {
         console.error('Error fetching combined columns:', error);
@@ -27,8 +36,21 @@ export class Gk2022Component implements OnInit {
     );
   }
 
+  updatePages() {
+    this.pages = [];
+    for (let i = 1; i <= this.totalPages; i++) {
+      this.pages.push(i);
+    }
+  }
 
+  goToPage(page: number) {
+    this.currentPage = page;
+    this.getDisplayedColumns();
+  }
 
-
-
+  getDisplayedColumns() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.displayedColumns = this.combinedColumns.slice(startIndex, endIndex);
+  }
 }
